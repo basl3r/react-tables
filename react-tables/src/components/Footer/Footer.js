791516b76d5d93c77f4a32b7bce5722c062df7cd
basl3r <ajgrociak@gmail.com>
2024-02-16 18:57:43 +0100

    Create Footer component and style it

---
+++ react-tables/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <span className={styles.footer} >Copyright &copy; PizzeriaApp 2024 </span>
+  )
+};
+
+export default Footer;
