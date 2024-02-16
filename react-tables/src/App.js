7a8e98d68ec715a1f2dc2b0c0809f96a5633b9a5
basl3r <ajgrociak@gmail.com>
2024-02-16 19:28:56 +0100

    Import global styles

--- react-tables/src/App.js
+++ react-tables/src/App.js
@@ -1,8 +1,17 @@
 import Nav from "./components/Nav/Nav";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Nav />
+    <>
+      <Nav />
+      <Container>
+        <Content />
+        <Footer />
+      </Container>
+    </>
   );
 }
 
--- react-tables/src/index.js
+++ react-tables/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
